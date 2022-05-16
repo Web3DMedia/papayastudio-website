e1f6bc87008b5f2a0ee139f24f8a1b6f55b0207e
maganijnr <maganijoshua25@gmail.com>
2022-05-16 08:30:37 +0100

    hero section build

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,8 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-const handler = (req, res) => {
+const hello = (req, res) => {
   res.statusCode = 200;
   res.json({ name: "John Doe" });
 };
 
-export default handler
+export default hello;
