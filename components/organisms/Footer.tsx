c847ad194cae387ec4bc9f4cb8af8830aff87357
Martins Ikpe <martinsikpe25@gmail.com>
2022-05-22 20:40:45 +0100

    Fix: Socials on mobile view

--- components/organisms/Footer.tsx
+++ components/organisms/Footer.tsx
@@ -76,6 +76,8 @@ top: 0px;
 @media (max-width: 1024px) {
 width: 428px;
 height:477px;
+right: 60px;
+top:-30px;
 }
 @media (max-width: 600px) {
 width: 215px;
@@ -91,16 +93,19 @@ width:548px;
 transform: scaleX(-1);
 right: -10px;
 @media (max-width: 1024px) {
+top: -110px;
+width:316px;
 }
 @media (max-width: 600px) {
-
+width:195px;
+top: 80px;
 }
 `
 const Circle = styled.div`
 margin: 0 18px;
 width: 5px;
 height: 5px;
-background: #FF6661;
+background: var(--PrimaryOne);
 border-radius: 50%;
 `
 const Socials = styled.div`
@@ -115,6 +120,9 @@ margin-left: 120px;
 z-index:2;
 &:nth-child(1) {
     margin-left: 0px;
+}
+&:nth-child(5) {
+    margin-left: -40px;
 }
 @media (max-width: 1024px) {
 margin-left: 70px;
@@ -145,7 +153,7 @@ const Footer = () => {
         <div className='relative'>
             <FooterContainer className='px-6 xl:pl-24 xl:px-0 relative'>
 
-                <div className='pt-12 lg:pt-28'>
+                <div className='pt-16 xl:pt-28'>
                     <Image src="/assets/footer-logo.png" width={127} height={35} alt="papaya" objectFit='contain' />
 
                     <FirstText className='text-footerText'>
@@ -169,7 +177,7 @@ const Footer = () => {
                                 <Socials>Instagram</Socials>
                                 <Socials>Linkedln</Socials>
                                 <Socials>Opensea</Socials>
-                                <Socials className='md:hidden flex items-center'> <Circle className='p-1'></Circle> Legal</Socials>
+                                <Socials className='md:hidden flex items-center ml-2'> <Circle className='p-1'></Circle> Legal</Socials>
                             </SocialContainer>
                         </div>
                         
@@ -203,7 +211,7 @@ const Footer = () => {
             </FooterContainer >
             
             <Mascot>
-                <Image src="/assets/png1.png" width={548} height={548} objectFit="cover" alt="papaya mascot" />
+                <Image src="/assets/footer-mascot.png" width={548} height={548} objectFit="contain" alt="papaya mascot" />
             </Mascot>
         </div>
     )
