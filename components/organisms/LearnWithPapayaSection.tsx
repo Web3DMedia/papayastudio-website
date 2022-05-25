d06830a49fe43bdb02559d41950c7dcdcdb1553b
Martins Ikpe <martinsikpe25@gmail.com>
2022-05-25 11:21:51 +0100

    updated tab view for learn with papaya section

--- components/organisms/LearnWithPapayaSection.tsx
+++ components/organisms/LearnWithPapayaSection.tsx
@@ -40,7 +40,8 @@ width:55%;
 font-size: 20px; 
 }
 @media (max-width: 1024px) {
-font-size: 18px; 
+font-size: 18px;
+width:90%;
 }
 `
 const FourthText = styled.h4`
@@ -48,7 +49,8 @@ font-weight: 400;
 font-size: 18px;
 line-height: 30px;
 @media (max-width: 1024px) {
-font-size: 14px;
+font-size: 16px;
+margin-bottom:90px;
 }`
 const Aya = styled.div`
 position: absolute;
@@ -60,6 +62,7 @@ bottom: 0;
 @media (max-width: 600px) {
 width: 209px;
 height:229px;
+bottom:-20px;
 }
 `
 const Pap = styled.div`
@@ -92,13 +95,27 @@ right: 50px;
 }
 `
 const Im = styled.div`
+position:relative;
 margin-left:360px;
+z-index:2;
+@media (max-width: 1024px) {
+margin-left:145px;
+}
+@media (max-width: 800px) {
+margin-left:20px;
+}
 `
 const Span = styled.div<Props>`
 background: url(${({imgs}) => imgs ? `${imgs}` : ''});
 background-size: cover;
 margin-left: 34px;
-transition: background 0.3s;
+transition: background 0.2s;
+@media (max-width: 1024px) {
+margin-left:20px;
+}
+@media (max-width: 700px) {
+margin-left: 12px;
+}
 &:nth-child(1){
 width:122px;
 height:111px;
@@ -124,42 +141,58 @@ background-position-y: -95px;
 @media (max-width: 1024px) {
  &:nth-child(1){
 width:88px;
- height:96px;
-background-position-x: 0;
+height:96px;
+background-size: 900px;
+background-position-x: -40px;
+background-position-y: -225px;
 }
 &:nth-child(2){
 width:421px;
- height: 231px;
-background-position-x: -75px;
+height: 231px;
+background-size: 900px;
+background-position-x: -175px;
 }
 &:nth-child(3){
-width:117px;
+width:167px;
 height: 192px;
-background-position-x: -150px;
+background-size: 900px;
+background-position-x: 270px;
+background-position-y: -85px;
 }
 
 @media (max-width: 600px) {
  &:nth-child(1){
 width:36px;
  height:39px;
+background-size: 400px;
 background-position-x: 0;
 }
 &:nth-child(2){
 width:170px;
- height: 93px;
-background-position-x: -75px;
+height: 93px;
+background-size: 325px;
+background-position-x: -45px;
+background-position-y: -15px;
 }
 &:nth-child(3){
 width:71px;
 height: 78px;
-background-position-x: -150px;
+background-size: 325px;
+background-position-x: -225px;
+background-position-y: -30px;
 }
 `
 const SpanTwo = styled.div<Props>`
 background: url(${({imgs}) => imgs ? `${imgs}` : ''});
 background-size: cover;  
 margin-left: 34px;
-transition: background 0.3s;
+transition: background 0.2s;
+@media (max-width: 1024px) {
+margin-left:20px;
+}
+@media (max-width: 600px) {
+margin-left: 12px;
+}
 &:nth-child(1){
 margin-left: 0px;
 width:521px;
@@ -182,44 +215,52 @@ background-position-y: -370px;
  &:nth-child(1){
 width:427px;
 height:193px;
-background-position-x: 0;
+background-size: 900px;
+background-position-x: -30px;
+background-position-y: -320px;
 }
 &:nth-child(2){
 width:421px;
- height: 232px;
-background-position-x: -75px;
+height: 232px;
+background-size: 900px;
+background-position-x: -540px;
+background-position-y: -320px;
 }
 
 @media (max-width: 600px) {
  &:nth-child(1){
 width:172px;
-height:78px;
-background-position-x: 0;
+height:18px;
+background-size: 500px;
+background-position-x: 30%;
+background-position-y: -290px;
 }
 &:nth-child(2){
 width:170px;
- height: 93px;
-background-position-x: -75px;
+height: 93px;
+background-size: 600px;
+background-position-x: -540px;
+background-position-y: -320px;
 }
 `
 const LearnWithPapayaSection = () => {
-    const [bg, setBg] = useState<Number>(2)
+    const [bg, setBg] = useState<Number>(1)
     
-    useEffect(() => {
-        const interval = setInterval(() => {
-            setBg(bg => Number(bg) + Number(1) > 3 ? 1 : Number(bg) + Number(1))
-    }, 5000)
-    return () => {
-      clearInterval(interval)
-    }
-  }, [])
+     useEffect(() => {
+         const interval = setInterval(() => {
+             setBg(bg => Number(bg) + Number(1) > 3 ? 1 : Number(bg) + Number(1))
+     }, 5000)
+     return () => {
+       clearInterval(interval)
+     }
+}, [])
   return (
     <LearnWithPapayaContainer className='font-heading relative'>
         <FirstText className='pt-24 pl-6 md:pl-20 pb-12 text-primary3'>LEARN <br/>  WITH PAPAYAS</FirstText>
 
         <SecondText className='pl-6 md:pl-20 xl:pl-40 text-learnpapayatext'>Join Our Learning Academy</SecondText>
 
-        <div className='pl-56 pt-11'>
+        <div className='pl-6 md:pl-20 xl:pl-56 pt-11'>
             <ThirdText className=' text-learnpapayatext2'>At Papaya Studios we are one of the best in the utmost ultimate universe if we are going to the summmer intern product good best inspire gorgueous</ThirdText>
 
             <FourthText className='flex items-center text-learnpapayatext pt-6'> Contact Us <img src='/assets/component-btn.png' alt='Linkedln' className='ml-2 w-6 h-6'/>  </FourthText>
@@ -231,7 +272,7 @@ const LearnWithPapayaSection = () => {
                 <Span imgs={`/assets/Learn${bg}.jpeg`}></Span>            
                 <Span imgs={`/assets/Learn${bg}.jpeg`}></Span>            
             </div>
-            <div className='flex pt-10'>
+            <div className='flex pt-3 md:pt-5 xl:pt-10'>
                 <SpanTwo imgs={`/assets/Learn${bg}.jpeg`}></SpanTwo>            
                 <SpanTwo imgs={`/assets/Learn${bg}.jpeg`}></SpanTwo>
             </div>            
