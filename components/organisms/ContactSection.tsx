eca85d9209c5c0f1cb476a1813b1ee1731a0d914
Martins Ikpe <martinsikpe25@gmail.com>
2022-08-01 23:47:41 +0100

    Contact Section UI updates

--- components/organisms/ContactSection.tsx
+++ components/organisms/ContactSection.tsx
@@ -8,8 +8,8 @@ const ContactSection = () => {
             <div className="bgTransformationOneLeft"/>
             <div className="bgTransformationOneRight"/>
          </div>
-         <div className="w-full h-full flex flex-col items-center justify-center">
-            <h3 className="text-[#23262E] font-normal text-xs uppercase mt-4 mb-6">Get in touch with us</h3>
+         <div className="w-full h-full flex flex-col lg:items-center lg:justify-center">
+            <h3 className="text-[#23262E] font-normal text-xs uppercase mt-4 mb-6 text-center">Get in touch with us</h3>
             <ContactForm/>
          </div>
       </div>
