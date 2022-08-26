04f5e771ed45cf5b351dea3fa45262daf6df63b2
Martins Ikpe <martinsikpe25@gmail.com>
2022-08-26 03:38:36 +0100

    Updates to MetaTag Images

--- components/atoms/MetaTag.tsx
+++ components/atoms/MetaTag.tsx
@@ -11,7 +11,7 @@ const MetaTag = ({ tagName }: IProps) => {
                 <meta
                     name="description"
                     content="At Papayas studios we are at the forefront of decentralized content creation, helping to simplify the complexities of Blockchain and emerging technologies." />
-                <link rel="icon" href="/assets/logo.svg" />
+                <link rel="icon" href="https://staging.papayas.studio//assets/logo.svg" />
 
                 <meta name="title" content="we are at the forefront of decentralized content creation."></meta>
                 <meta name="description" content="helping to simplify the complexities of Blockchain and emerging technologies."></meta>
@@ -20,14 +20,14 @@ const MetaTag = ({ tagName }: IProps) => {
                 <meta property="og:url" content="https://staging.papayas.studio/"></meta>
                 <meta property="og:title" content="we are at the forefront of decentralized content creation."></meta>
                 <meta property="og:description" content="helping to simplify the complexities of Blockchain and emerging technologies."></meta>
-                <meta property="og:image" content="https://staging.papayas.studio//assets/papito-and-papina-new-two.jpeg"></meta>
+                <meta property="og:image" content="https://staging.papayas.studio//assets/papayas-studio-logo.png"></meta>
 
 
                 <meta property="twitter:card" content="summary_large_image"></meta>
                 <meta property="twitter:url" content="https://staging.papayas.studio/"></meta>
                 <meta property="twitter:title" content="we are at the forefront of decentralized content creation."></meta>
                 <meta property="twitter:description" content="helping to simplify the complexities of Blockchain and emerging technologies."></meta>
-                <meta property="twitter:image" content="https://staging.papayas.studio//assets/papito-and-papina-new-two.jpeg"></meta>
+                <meta property="twitter:image" content="https://staging.papayas.studio//assets/papayas-studio-logo.png"></meta>
             </Head>
         </>
     )
