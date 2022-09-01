e46a9051d64272ae269b5f0dfe444862c0f287e1
Martins Ikpe <martinsikpe25@gmail.com>
2022-09-01 05:41:20 +0100

    Implemented Loading Screen

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,9 +5,22 @@ import Theme from "../styles/Theme";
 import { ThemeProvider } from "@wigxel/react-components";
 import Head from "next/head";
 import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3";
+import { useRouter } from 'next/router';
+import LoadingScreen from "../components/molecules/LoadingScreen";
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  const [pageLoading, setPageLoading] = React.useState<boolean>(false);
+  React.useEffect(() => {
+    const handleStart = () => { setPageLoading(true); };
+    const handleComplete = () => { setPageLoading(false); };
+
+    router.events.on('routeChangeStart', handleStart);
+    router.events.on('routeChangeComplete', handleComplete);
+    router.events.on('routeChangeError', handleComplete);
+  }, [router]);
+
   return (
     <>
       <Head>
@@ -17,19 +30,21 @@ export default function MyApp({ Component, pageProps }) {
           content="width=device-width, initial-scale=1.0"
         />
       </Head>
-      <GoogleReCaptchaProvider
-        reCaptchaKey={process.env.NEXT_PUBLIC_TEST_SITE_KEY}
-        scriptProps={{
-          async: false,
-          defer: false,
-          appendTo: "head",
-          nonce: undefined,
-        }}
-      >
-        <ThemeProvider theme={Theme}>
-          <Component {...pageProps} />
-        </ThemeProvider>
-      </GoogleReCaptchaProvider>
+      {
+        pageLoading ? (<LoadingScreen></LoadingScreen>) : <GoogleReCaptchaProvider
+          reCaptchaKey={process.env.NEXT_PUBLIC_TEST_SITE_KEY}
+          scriptProps={{
+            async: false,
+            defer: false,
+            appendTo: "head",
+            nonce: undefined,
+          }}
+        >
+          <ThemeProvider theme={Theme}>
+            <Component {...pageProps} />
+          </ThemeProvider>
+        </GoogleReCaptchaProvider>
+      }
 
     </>
   );
