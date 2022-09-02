7f2752be854a87661b012aa584722a7e5201daed
Martins Ikpe <martinsikpe25@gmail.com>
2022-09-02 13:26:36 +0100

    Added the background transformation

--- components/organisms/ContactSection.tsx
+++ components/organisms/ContactSection.tsx
@@ -1,16 +1,14 @@
 import React from 'react'
+import BgTransfromation from '../atoms/BgTransfromation'
 import ContactForm from '../molecules/ContactForm'
 
 const ContactSection = () => {
    return (
       <div className=" min-h-[60vh]  mb-5">
-         <div className="bgTransformationOne">
-            <div className="bgTransformationOneLeft"/>
-            <div className="bgTransformationOneRight"/>
-         </div>
+         <BgTransfromation></BgTransfromation>
          <div className="w-full h-full flex flex-col md:items-center md:justify-center">
             <h3 className="text-[#23262E] font-normal text-xs uppercase mt-4 mb-6 text-center">Get in touch with us</h3>
-            <ContactForm/>
+            <ContactForm />
          </div>
       </div>
    )
