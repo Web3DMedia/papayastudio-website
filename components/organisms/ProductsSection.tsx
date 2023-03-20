3cad8d4ab87a14d3b24f9c51e06115d52510ad9b
Martins Ikpe <martins.ikpe@stu.cu.edu.ng>
2023-03-20 23:06:07 +0100

    style: remove padding

--- components/organisms/ProductsSection.tsx
+++ components/organisms/ProductsSection.tsx
@@ -135,7 +135,7 @@ const ProductsSection = ({ myRefThree, isThree }: IProps) => {
                 </div>
               </div>
               <Link href="/our-works" passHref>
-                <HeaderText className="px-5 md:pl-04 lg:pl-[85px] pt-[36px] text-[18px] leading-[30px] font-[700] cursor-light">View all works <span className="ml-[12px] bg-primary1 px-[7px] rounded-full">&gt;</span> </HeaderText>
+                <HeaderText className="px-5 md:pl-04 lg:pl-[85px] xl:pl-0  pt-[36px] text-[18px] leading-[30px] font-[700] cursor-light">View all works <span className="ml-[12px] bg-primary1 px-[7px] rounded-full">&gt;</span> </HeaderText>
               </Link>
             </div>
           </div>
